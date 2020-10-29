0b366824077dda258a1dba4177e3c171fd228b5f
Debojit Roy <debojit.roy@unohomeloans.com.au>
2020-10-29 12:42:01 +1100

    Setting up React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import NavbarBrand from 'react-bootstrap/NavbarBrand'
+import "./App.css";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Navbar collapseOnSelect>
+        <header>
+          <NavbarBrand>
+            <Link to="/">Scratch</Link>
+          </NavbarBrand>
+          <Navbar.Toggle />
+        </header>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
