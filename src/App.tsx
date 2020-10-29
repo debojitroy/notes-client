9425c68fa24a1b9d97b9a669dda4bfa3eca5ddc0
Debojit Roy <debojit.roy@unohomeloans.com.au>
2020-10-29 13:57:56 +1100

    Adding Nav Bar and Base Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Navbar from "react-bootstrap/Navbar";
-import NavbarBrand from 'react-bootstrap/NavbarBrand'
+import NavbarBrand from "react-bootstrap/NavbarBrand";
+import NavItem from "react-bootstrap/NavItem";
+import { LinkContainer } from "react-router-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 
 function App() {
   return (
     <div className="App container">
-      <Navbar collapseOnSelect>
-        <header>
-          <NavbarBrand>
-            <Link to="/">Scratch</Link>
-          </NavbarBrand>
-          <Navbar.Toggle />
-        </header>
-      </Navbar>
+      <div className="nav-container">
+        <NavbarBrand>
+          <Link to="/">Scratch</Link>
+        </NavbarBrand>
+        <LinkContainer className="move-right" to="/signup">
+          <NavItem>Signup</NavItem>
+        </LinkContainer>
+        <LinkContainer to="/login">
+          <NavItem>Login</NavItem>
+        </LinkContainer>
+      </div>
       <Routes />
     </div>
   );
